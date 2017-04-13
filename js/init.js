a5ae206d854649bc0d680b1201e8ace2b816aec6
Danielle Midulla <danielle.midulla@gmail.com>
2017-04-13 16:10:13 -0400

    added parallax

--- js/init.js
+++ js/init.js
@@ -1,9 +1,11 @@
 // code from:http://materializecss.com/modals.html#!
 // additional help from: http://stackoverflow.com/questions/41626343/materialized-modal-not-displaying
 // parallax code from: http://materializecss.com/parallax.html
-// this initializes all modals so they can be triggered
 $(document).ready(function() {
   //initializing code goes here!
   $(".dropdown-button").dropdown({ hover: false });
+  // this initializes all modals so they can be triggered
   $('.modal').modal();
+  // parallax code from: http://materializecss.com/parallax.html
+  $('.parallax').parallax();
 });
